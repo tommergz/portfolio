e9fb5e79d0d756dc14de6725953de853bb96dfbb
Artem Lipchik <tommerg09@gmail.com>
2019-10-09 15:03:14 +0300

    make my portfolio

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+
+const buttons = {
+    portfolio_btn: document.getElementById('portfolio-btn'),
+    skills_btn: document.getElementById('skills-btn'),
+}
+
+const lamps = {
+    portfolio_lamp: document.getElementById('portfolio-lamp'),
+    skills_lamp: document.getElementById('skills-lamp'),
+}
+
+buttons.portfolio_btn.addEventListener('click', () => {
+    lamps.skills_lamp.style.backgroundColor = 'black';
+    lamps.portfolio_lamp.style.backgroundColor = '#00ff00';
+    document.getElementById('skills-container').style.display = 'none';
+    document.getElementById('portfolio-container').style.display = 'block';  
+})
+
+buttons.skills_btn.addEventListener('click', () => {
+    lamps.portfolio_lamp.style.backgroundColor = 'black';
+    lamps.skills_lamp.style.backgroundColor = '#00ff00';
+    document.getElementById('portfolio-container').style.display = 'none';
+    document.getElementById('skills-container').style.display = 'block';
+})
+
+
