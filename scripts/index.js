9ccc27e884eb93e105a70b9c4848574713cf704b
Artem <tommerg09@gmail.com>
2020-04-08 13:09:05 +0300

    localstorage

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,31 @@
-
-if (localStorage.block === 'skills') {
-    document.getElementById('portfolio-container').style.display = 'none';
-    document.getElementById('skills-container').style.display = 'block';
-}
-
 const buttons = {
     portfolio_btn: document.getElementById('portfolio-btn'),
     skills_btn: document.getElementById('skills-btn'),
 }
+
+const portfolioBtn = document.getElementById('portfolio-btn');
+const skills_btn = document.getElementById('skills-btn');
 
 const lamps = {
     portfolio_lamp: document.getElementById('portfolio-lamp'),
     skills_lamp: document.getElementById('skills-lamp'),
 }
 
-buttons.portfolio_btn.addEventListener('click', () => {
-    lamps.skills_lamp.style.backgroundColor = 'black';
-    lamps.portfolio_lamp.style.backgroundColor = '#00ff00';
-    document.getElementById('skills-container').style.display = 'none';
-    document.getElementById('portfolio-container').style.display = 'block'; 
-    localStorage.setItem('block', 'projects'); 
-})
+const activateBlock = () => {
+    if(event) { 
+        if (event.currentTarget.id === 'portfolio-btn') { localStorage.setItem('skills', false) }
+        else if (event.currentTarget.id === 'skills-btn') { localStorage.setItem('skills', true) }
+    };
+    const skillsBlock = JSON.parse(localStorage.skills);
+    lamps.portfolio_lamp.style.backgroundColor = skillsBlock ? 'black' : '#00ff00';
+    lamps.skills_lamp.style.backgroundColor = skillsBlock ? '#00ff00' : 'black';
+    document.getElementById('portfolio-container').style.display = skillsBlock ? 'none': 'block';
+    document.getElementById('skills-container').style.display = skillsBlock ? 'block' : 'none';
+}
 
-buttons.skills_btn.addEventListener('click', () => {
-    lamps.portfolio_lamp.style.backgroundColor = 'black';
-    lamps.skills_lamp.style.backgroundColor = '#00ff00';
-    document.getElementById('portfolio-container').style.display = 'none';
-    document.getElementById('skills-container').style.display = 'block';
-    localStorage.setItem('block', 'skills');
-})
+buttons.portfolio_btn.addEventListener('click', activateBlock);
+buttons.skills_btn.addEventListener('click', activateBlock);
 
-
+if (localStorage.skills === 'true') {
+    activateBlock();
+}
