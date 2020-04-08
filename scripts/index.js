774046c0faf959e2bd59c1c32d5e8161e592fd4c
Artem <tommerg09@gmail.com>
2020-04-08 10:59:07 +0300

    new skill block

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,9 @@
 
+if (localStorage.block === 'skills') {
+    document.getElementById('portfolio-container').style.display = 'none';
+    document.getElementById('skills-container').style.display = 'block';
+}
+
 const buttons = {
     portfolio_btn: document.getElementById('portfolio-btn'),
     skills_btn: document.getElementById('skills-btn'),
@@ -13,7 +18,8 @@ buttons.portfolio_btn.addEventListener('click', () => {
     lamps.skills_lamp.style.backgroundColor = 'black';
     lamps.portfolio_lamp.style.backgroundColor = '#00ff00';
     document.getElementById('skills-container').style.display = 'none';
-    document.getElementById('portfolio-container').style.display = 'block';  
+    document.getElementById('portfolio-container').style.display = 'block'; 
+    localStorage.setItem('block', 'projects'); 
 })
 
 buttons.skills_btn.addEventListener('click', () => {
@@ -21,6 +27,7 @@ buttons.skills_btn.addEventListener('click', () => {
     lamps.skills_lamp.style.backgroundColor = '#00ff00';
     document.getElementById('portfolio-container').style.display = 'none';
     document.getElementById('skills-container').style.display = 'block';
+    localStorage.setItem('block', 'skills');
 })
 
 
